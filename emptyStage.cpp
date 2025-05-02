0492f55c585b6158da97d31be8f40f0d036e9572
MJ1251 <11308571+MicDropBaam@users.noreply.github.com>
2025-05-01 20:14:23 -0700

    Update emptyStage.cpp

--- emptyStage.cpp
+++ emptyStage.cpp
@@ -1,17 +1,3 @@
-/* ---------------------------------------------------------------------------
-Copyright (C) 2017 DigiPen Institute of Technology.
-Reproduction or disclosure of this file or its contents without the prior
-written consent of DigiPen Institute of Technology is prohibited.
-
-File Name:      emptyStage.cpp
-Purpose:        This is just used as a quick hack to pause the game when we alt tab
-Language:       C++
-Project:        GAM200
-Author:         Name : Stuart Sulaski
-Email:          stuartsulaski@gmail.com
-Creation date:  2017.06.12
-----------------------------------------------------------------------------*/
-
 #include <emptyStage.hpp>
 
 BE::Stage::emptyStage::emptyStage()
